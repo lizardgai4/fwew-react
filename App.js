85bda20c9072d2c87335967a912dd609636481eb
Corey Scheideman <corscheid@gmail.com>
2020-07-06 18:14:36 -0400

    Clean up imports

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import { StatusBar, StyleSheet, View } from "react-native";
+import { StatusBar, View } from "react-native";
 
 // import UI components
 import ActionBar from "./ui/action-bar";
-import Entry from "./ui/entry";
 import ResultView from "./ui/result-view";
 import styles from "./ui/styles";
 
