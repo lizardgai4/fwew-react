5523dda337d0eb2765bec73601d2458e2e1e2f63
Corey Scheideman <corscheid@gmail.com>
2020-07-08 02:31:28 -0400

    Add bottom navigation tabs

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, { useReducer } from "react";
-import { StatusBar, View, Text } from "react-native";
+import { StatusBar, View } from "react-native";
 import update from "immutability-helper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 
 // Import Context
 import { AppContext } from "./AppContext";
@@ -30,7 +33,20 @@ function reducer(state, action) {
   }
 }
 
-export default function App() {
+function FwewScreen() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#7494BA" />
+      <AppContext.Provider value={{ state, dispatch }}>
+        <ActionBar search />
+        <ResultView />
+      </AppContext.Provider>
+    </View>
+  );
+}
+
+function ListScreen() {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <View style={styles.container}>
@@ -40,5 +56,78 @@ export default function App() {
         <ResultView />
       </AppContext.Provider>
     </View>
+  );
+}
+
+function RandomScreen() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#7494BA" />
+      <AppContext.Provider value={{ state, dispatch }}>
+        <ActionBar />
+        <ResultView />
+      </AppContext.Provider>
+    </View>
+  );
+}
+
+function NumberScreen() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#7494BA" />
+      <AppContext.Provider value={{ state, dispatch }}>
+        <ActionBar search />
+        <ResultView />
+      </AppContext.Provider>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          switch (route.name) {
+            case "Fwew":
+              iconName = "search";
+              break;
+            case "List":
+              iconName = "list";
+              break;
+            case "Random":
+              iconName = "help-circle";
+              break;
+            case "Number":
+              iconName = "hash";
+              break;
+          }
+          return <Feather name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        // `activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Fwew" component={FwewScreen} />
+      <Tab.Screen name="List" component={ListScreen} />
+      <Tab.Screen name="Random" component={RandomScreen} />
+      <Tab.Screen name="Number" component={NumberScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   );
 }
