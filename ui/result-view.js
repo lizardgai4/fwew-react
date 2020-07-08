753890a0af119810ee9bc4657b2929f0a5a6c11a
Corey Scheideman <corscheid@gmail.com>
2020-07-08 05:28:17 -0400

    move styles in, make api calls according to props

--- ui/result-view.js
+++ ui/result-view.js
@@ -1,15 +1,25 @@
 import React, { useContext, useEffect } from "react";
-import { ActivityIndicator, FlatList, SafeAreaView, Text } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import axios from "axios";
 import Entry from "./entry";
-import styles from "./styles";
 
 // Import Context
 import { AppContext } from "../AppContext";
 
-export default function ResultView() {
+export default function ResultView(props) {
   const { state, dispatch } = useContext(AppContext);
-  const ApiUrlFwew = "https://tirea.learnnavi.org/api/fwew/";
+  const ApiUrl = "https://tirea.learnnavi.org/api";
+  const ApiUrlFwew = `${ApiUrl}/fwew/`;
+  const ApiUrlFwewReverse = `${ApiUrl}/fwew/r/`;
+  const ApiUrlList = `${ApiUrl}/list/`;
+  const ApiUrlRandom = `${ApiUrl}/random/`;
 
   const changeDataValue = (newValue) => {
     dispatch({ type: "UPDATE_DATA", data: newValue });
@@ -20,9 +30,35 @@ export default function ResultView() {
   };
 
   const getDataFromFwewApi = async () => {
+    let numRandom = 10; // stub
+    let args = ""; // stub
+    let url;
+    switch (props.endpoint) {
+      case "fwew":
+        url = ApiUrlFwew;
+        break;
+      case "fwewReverse":
+        url = ApiUrlFwewReverse;
+        break;
+      case "list":
+        url = ApiUrlList;
+        if (args) {
+          url += args;
+        }
+        break;
+      case "random":
+        url = ApiUrlRandom;
+        if (numRandom) {
+          url += numRandom + "/";
+          if (args) {
+            url += args;
+          }
+        }
+        break;
+    }
     let response;
     try {
-      response = await axios.get(ApiUrlFwew + state.inputText);
+      response = await axios.get(url + state.inputText);
     } catch (e) {
       response = { data: [] };
     }
@@ -43,12 +79,14 @@ export default function ResultView() {
   if (state.isLoading) {
     content = <ActivityIndicator style={{ marginTop: 16 }} />;
   } else if (state.data.message != null) {
+    // 404 or other error, usually no result
     content = (
-      <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
         <Text>{state.data.message + ": " + state.inputText}</Text>
-      </SafeAreaView>
+      </View>
     );
   } else if (state.data.search_url != null) {
+    // hit the root endpoint of the api, ignore the response
     content = <Text>{""}</Text>;
   } else {
     content = (
@@ -70,3 +108,18 @@ export default function ResultView() {
 
   return <SafeAreaView style={styles.list}>{content}</SafeAreaView>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  list: {
+    width: "100%",
+  },
+  contentContainer: {
+    marginTop: 8,
+    paddingBottom: 72,
+  },
+});
