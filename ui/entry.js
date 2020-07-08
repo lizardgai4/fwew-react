07d6c5b57680523021b9fb919c6b8ef1f1a81fc9
Corey Scheideman <corscheid@gmail.com>
2020-07-08 05:25:25 -0400

    rewrite as function component, move styles in

--- ui/entry.js
+++ ui/entry.js
@@ -1,45 +1,48 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { Entypo } from "@expo/vector-icons";
-import styles from "./styles";
+import EntryIndex from "./entry-index";
 
-class EntryIndex extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <View style={styles.entry_index}>
-        <Text style={styles.entry_number}>{this.props.number}</Text>
-      </View>
-    );
-  }
+export default function Entry(props) {
+  return (
+    <View style={styles.entry}>
+      <EntryIndex number={props.number} />
+      <Text selectable={true} style={styles.entry_navi}>
+        {props.navi}
+      </Text>
+      <Text selectable={true} style={styles.entry_pos}>
+        {props.pos}
+      </Text>
+      <Text selectable={true} style={styles.entry_en}>
+        {props.en}
+      </Text>
+      <Entypo
+        style={styles.entry_chevron}
+        name="chevron-thin-right"
+        size={24}
+        color="black"
+      />
+    </View>
+  );
 }
 
-export default class Entry extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <View style={styles.entry}>
-        <EntryIndex number={this.props.number} />
-        <Text selectable={true} style={styles.entry_navi}>
-          {this.props.navi}
-        </Text>
-        <Text selectable={true} style={styles.entry_pos}>
-          {this.props.pos}
-        </Text>
-        <Text selectable={true} style={styles.entry_en}>
-          {this.props.en}
-        </Text>
-        <Entypo
-          style={styles.entry_chevron}
-          name="chevron-thin-right"
-          size={24}
-          color="black"
-        />
-      </View>
-    );
-  }
-}
+const styles = StyleSheet.create({
+  entry: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderWidth: 1,
+    borderRadius: 16,
+    borderColor: "#ddd",
+    flexDirection: "row",
+    overflow: "hidden",
+  },
+  entry_navi: {
+    fontWeight: "bold",
+    fontSize: 24,
+    marginLeft: 16,
+  },
+  entry_pos: { fontStyle: "italic", fontSize: 14, marginLeft: 8, marginTop: 8 },
+  entry_en: { fontSize: 14, marginLeft: 8, marginTop: 8 },
+  entry_chevron: { marginLeft: "auto" },
+});
