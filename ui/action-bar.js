fc43ed57b957387a3f08a8b5a5fcf5a1edc500bc
Corey Scheideman <corscheid@gmail.com>
2020-07-08 01:32:31 -0400

    Add condition to search bar

--- ui/action-bar.js
+++ ui/action-bar.js
@@ -6,24 +6,32 @@ import styles from "./styles";
 // Import Context
 import { AppContext } from "../AppContext";
 
-export default function ActionBar() {
+export default function ActionBar(props) {
   const { state, dispatch } = useContext(AppContext);
   const changeInputValue = (newValue) => {
     dispatch({ type: "UPDATE_INPUT", data: newValue });
   };
 
+  if (props.search) {
+    return (
+      <View style={styles.action_bar}>
+        <Image source={fwew} style={styles.icon} />
+        <TextInput
+          value={state.inputText}
+          onChangeText={(query) => changeInputValue(query)}
+          placeholder={"search..."}
+          autoCapitalize={"none"}
+          autoCompleteType={"off"}
+          autoCorrect={false}
+          style={styles.input}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.action_bar}>
       <Image source={fwew} style={styles.icon} />
-      <TextInput
-        value={state.inputText}
-        onChangeText={(query) => changeInputValue(query)}
-        placeholder={"search..."}
-        autoCapitalize={"none"}
-        autoCompleteType={"off"}
-        autoCorrect={false}
-        style={styles.input}
-      />
     </View>
   );
 }
