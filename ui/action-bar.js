540851e2ac822553135588f183e7a3a73f763fec
Corey Scheideman <corscheid@gmail.com>
2020-07-08 05:24:41 -0400

    remove autoCompleteType prop, move styles in

--- ui/action-bar.js
+++ ui/action-bar.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
-import { Image, TextInput, View } from "react-native";
+import { Image, StyleSheet, TextInput, View } from "react-native";
 import fwew from "../assets/fwew.png";
-import styles from "./styles";
 
 // Import Context
 import { AppContext } from "../AppContext";
@@ -21,7 +20,6 @@ export default function ActionBar(props) {
           onChangeText={(query) => changeInputValue(query)}
           placeholder={"search..."}
           autoCapitalize={"none"}
-          autoCompleteType={"off"}
           autoCorrect={false}
           style={styles.input}
         />
@@ -35,3 +33,22 @@ export default function ActionBar(props) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  action_bar: {
+    height: 60,
+    width: "100%",
+    backgroundColor: "#7494BA",
+    paddingTop: 4,
+    flexDirection: "row",
+  },
+  icon: { marginLeft: 8, width: 48, height: 48 },
+  input: {
+    height: 48,
+    width: "80%",
+    paddingLeft: 16,
+    marginLeft: 8,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+  },
+});
