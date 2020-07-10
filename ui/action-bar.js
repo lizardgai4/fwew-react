1c9b65f42f70302c0f0273c6ba9a4b0bf1b2a57e
Corey Scheideman <corscheid@gmail.com>
2020-07-09 20:12:06 -0400

    simplify action bar component

--- ui/action-bar.js
+++ ui/action-bar.js
@@ -1,54 +1,26 @@
-import React, { useContext } from "react";
-import { Image, StyleSheet, TextInput, View } from "react-native";
+import React from "react";
+import { Image, Platform, StyleSheet, View } from "react-native";
 import fwew from "../assets/fwew.png";
 
-// Import Context
-import { AppContext } from "../AppContext";
-
 export default function ActionBar(props) {
-  const { state, dispatch } = useContext(AppContext);
-  const changeInputValue = (newValue) => {
-    dispatch({ type: "UPDATE_INPUT", data: newValue });
-  };
-
-  if (props.search) {
-    return (
-      <View style={styles.action_bar}>
-        <Image source={fwew} style={styles.icon} />
-        <TextInput
-          value={state.inputText}
-          onChangeText={(query) => changeInputValue(query)}
-          placeholder={"search..."}
-          autoCapitalize={"none"}
-          autoCorrect={false}
-          style={styles.input}
-        />
-      </View>
-    );
-  }
-
   return (
     <View style={styles.action_bar}>
       <Image source={fwew} style={styles.icon} />
+      {props.children}
     </View>
   );
 }
 
+const APPBAR_HEIGHT = Platform.OS === "ios" ? 44 : 56;
+const ICON_SIZE = Platform.OS === "ios" ? 32 : 48;
+
 const styles = StyleSheet.create({
   action_bar: {
-    height: 60,
+    height: APPBAR_HEIGHT,
     width: "100%",
     backgroundColor: "#7494BA",
     paddingTop: 4,
     flexDirection: "row",
   },
-  icon: { marginLeft: 8, width: 48, height: 48 },
-  input: {
-    height: 48,
-    width: "80%",
-    paddingLeft: 16,
-    marginLeft: 8,
-    backgroundColor: "#fff",
-    borderRadius: 16,
-  },
+  icon: { marginLeft: 8, width: ICON_SIZE, height: ICON_SIZE },
 });
