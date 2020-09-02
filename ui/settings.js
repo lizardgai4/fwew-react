df2243b12d5cf9c8338f49b1bbe8141a953c570b
Corey Scheideman <corscheid@gmail.com>
2020-09-02 18:54:34 -0400

    split settings object into separate objects

--- ui/settings.js
+++ ui/settings.js
@@ -1,43 +1,74 @@
-export const Settings = {
+export const SettingsGlobal = {
   languageCode: "en",
-  fwew: {
-    isReverseEnabled: false,
-    posFilterText: "all",
+};
+export const SettingsFwew = {
+  isReverseEnabled: false,
+  posFilterText: "all",
+};
+export const SettingsList = {
+  word: {
+    has: "",
+    starts: "",
+    ends: "",
+    like: "",
   },
-  list: {
-    word: {
-      has: "",
-      starts: "",
-      ends: "",
-      like: "",
-    },
-    pos: {
-      is: "",
-      has: "",
-      starts: "",
-      ends: "",
-      like: "",
-    },
-    syllables: {
-      lessThan: "0",
-      lessThanEqual: "0",
-      equal: "0",
-      greaterThanEqual: "0",
-      greaterThan: "0",
-    },
-    stress: {
-      lessThan: "0",
-      lessThanEqual: "0",
-      equal: "0",
-      greaterThanEqual: "0",
-      greaterThan: "0",
-    },
-    words: {
-      first: "0",
-      last: "0",
-    },
+  pos: {
+    is: "",
+    has: "",
+    starts: "",
+    ends: "",
+    like: "",
   },
-  random: {
-    numRandomWords: "8",
+  syllables: {
+    lessThan: "0",
+    lessThanEqual: "0",
+    equal: "0",
+    greaterThanEqual: "0",
+    greaterThan: "0",
+  },
+  stress: {
+    lessThan: "0",
+    lessThanEqual: "0",
+    equal: "0",
+    greaterThanEqual: "0",
+    greaterThan: "0",
+  },
+  words: {
+    first: "0",
+    last: "0",
+  },
+};
+export const settingsRandom = {
+  numRandomWords: "8",
+  word: {
+    has: "",
+    starts: "",
+    ends: "",
+    like: "",
+  },
+  pos: {
+    is: "",
+    has: "",
+    starts: "",
+    ends: "",
+    like: "",
+  },
+  syllables: {
+    lessThan: "0",
+    lessThanEqual: "0",
+    equal: "0",
+    greaterThanEqual: "0",
+    greaterThan: "0",
+  },
+  stress: {
+    lessThan: "0",
+    lessThanEqual: "0",
+    equal: "0",
+    greaterThanEqual: "0",
+    greaterThan: "0",
+  },
+  words: {
+    first: "0",
+    last: "0",
   },
 };
