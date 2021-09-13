b707d528c0eb4723f8d8e064fd73c73f8dd6816e
Corey Scheideman <corscheid@gmail.com>
2021-09-12 23:09:26 -0500

    Update comments

--- ui/settings.js
+++ ui/settings.js
@@ -1,10 +1,47 @@
+/**
+ * This file is part of fwew-react. 
+ * fwew-react: Fwew Na'vi Dictionary app written using React Native
+ * Copyright (C) 2021  Corey Scheideman <corscheid@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Global settings
+ * 
+ * languageCode: the default language code of the application
+ */
 export const SettingsGlobal = {
   languageCode: "en",
 };
+
+/**
+ * Fwew Settings
+ * 
+ * isReverseEnabled: the default search direction
+ * posFilterText: the default value of the part of speech filter
+ */
 export const SettingsFwew = {
   isReverseEnabled: false,
   posFilterText: "all",
 };
+
+/**
+ * List Settings
+ * 
+ * values for each what/cond/spec in List
+ */
 export const SettingsList = {
   word: {
     has: "",
@@ -38,6 +75,12 @@ export const SettingsList = {
     last: "0",
   },
 };
+
+/**
+ * Random Settings
+ * 
+ * values for each what/cond/spec in Random
+ */
 export const settingsRandom = {
   numRandomWords: "8",
   word: {
