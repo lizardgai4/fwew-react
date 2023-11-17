4c58af3324bfb455b6ca247db98d18eb1ae72e5c
Corey Scheideman <corscheid@gmail.com>
2023-11-17 15:19:03 -0600

    [next] refactor list

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,24 +1,11 @@
 import { SearchBar } from "@/components/SearchBar";
 import { Text, View } from "@/components/Themed";
-import { ListOptionsCond } from "@/components/list/ListOptionsCond";
-import { ListOptionsWhat } from "@/components/list/ListOptionsWhat";
+import { ListOptions } from "@/components/list/ListOptions";
 import { FwewSearchResults } from "@/components/search/FwewSearchResults";
-import Colors from "@/constants/Colors";
-import {
-  ListExpressionAttribute,
-  ListMenuCond,
-  ListMenuItem,
-  WhatValue,
-} from "@/constants/List";
-import { Results } from "@/types/fwew";
+import type { Results } from "@/types/fwew";
 import { list } from "fwew.js";
-import { useEffect, useRef, useState } from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  useColorScheme,
-} from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function ListScreen() {
   const [query, setQuery] = useState("");
@@ -57,138 +44,12 @@ export default function ListScreen() {
   );
 }
 
-interface ListOptionsProps {
-  query: string;
-  onSelect: React.Dispatch<React.SetStateAction<string>>;
-  execute: () => void;
-}
-
-export function ListOptions({ query, onSelect, execute }: ListOptionsProps) {
-  const [mode, setMode] = useState<ListExpressionAttribute>("what");
-  const whatRef = useRef<ListMenuItem<WhatValue>>();
-  const colorScheme = useColorScheme();
-  const colors = Colors[colorScheme ?? "light"];
-
-  const andButtonDisabled =
-    query.length === 0 ||
-    query.trim().split(" ").length < 3 ||
-    (query.trim().split(" ").length + 1) % 4 !== 0 ||
-    query.endsWith("and");
-
-  const next = () => {
-    const transitionMap = {
-      what: "cond",
-      cond: "spec",
-      spec: "what",
-    } as const;
-    setMode((prev) => transitionMap[prev]);
-  };
-
-  const handleSelectWhat = (what: ListMenuItem<WhatValue>) => {
-    onSelect((prev) => (prev ? `${prev} ${what.value} ` : what.value));
-    whatRef.current = what;
-    next();
-  };
-
-  const handleSelectCond = (cond: ListMenuCond[WhatValue][number]) => {
-    onSelect((prev) => (prev ? `${prev} ${cond.value} ` : cond.value));
-    next();
-  };
-
-  const handleSelectionSpec = () => {
-    onSelect((prev) => (prev ? `${prev} and ` : ""));
-    next();
-  };
-
-  useEffect(() => {
-    if (query.includes("  ")) {
-      onSelect((prev) => prev.replace("  ", " "));
-    }
-    if (query === "") {
-      setMode("what");
-    }
-  }, [query]);
-
-  return (
-    <>
-      <Text style={styles.title}>LIST OPTIONS</Text>
-      {mode === "what" && <ListOptionsWhat onSelect={handleSelectWhat} />}
-      {mode === "cond" && (
-        <ListOptionsCond what={whatRef.current} onSelect={handleSelectCond} />
-      )}
-      {mode === "spec" && (
-        <View style={styles.specButtonContainer}>
-          <TouchableOpacity
-            onPress={handleSelectionSpec}
-            style={[
-              styles.button,
-              {
-                borderColor: andButtonDisabled
-                  ? colors.placeholder
-                  : colors.text,
-              },
-            ]}
-            disabled={andButtonDisabled}
-          >
-            <Text
-              style={{
-                color: andButtonDisabled ? colors.placeholder : colors.text,
-              }}
-            >
-              AND...
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={execute}
-            style={[
-              styles.button,
-              {
-                borderColor: andButtonDisabled
-                  ? colors.placeholder
-                  : colors.text,
-              },
-            ]}
-            disabled={andButtonDisabled}
-          >
-            <Text
-              style={{
-                color: andButtonDisabled ? colors.placeholder : colors.text,
-              }}
-            >
-              GO
-            </Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-    padding: 16,
-    marginTop: 10,
-  },
   resultCount: {
     padding: 16,
     alignSelf: "center",
-  },
-  specButtonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 16,
-  },
-  button: {
-    width: 80,
-    alignSelf: "center",
-    padding: 16,
-    alignItems: "center",
-    justifyContent: "center",
-    borderWidth: 1,
   },
 });
---
+++ components/list/ListOptionsSpec.tsx
@@ -0,0 +1,75 @@
+import { Text, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
+import { StyleSheet, TouchableOpacity, useColorScheme } from "react-native";
+
+type ListOptionsSpecProps = {
+  handleSelectionSpec: () => void;
+  andButtonDisabled: boolean;
+  execute: () => void;
+};
+
+export default function ListOptionsSpec({
+  handleSelectionSpec,
+  andButtonDisabled,
+  execute,
+}: ListOptionsSpecProps) {
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? "light"];
+
+  return (
+    <View style={styles.specButtonContainer}>
+      <TouchableOpacity
+        onPress={handleSelectionSpec}
+        style={[
+          styles.button,
+          {
+            borderColor: andButtonDisabled ? colors.placeholder : colors.text,
+          },
+        ]}
+        disabled={andButtonDisabled}
+      >
+        <Text
+          style={{
+            color: andButtonDisabled ? colors.placeholder : colors.text,
+          }}
+        >
+          AND...
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={execute}
+        style={[
+          styles.button,
+          {
+            borderColor: andButtonDisabled ? colors.placeholder : colors.text,
+          },
+        ]}
+        disabled={andButtonDisabled}
+      >
+        <Text
+          style={{
+            color: andButtonDisabled ? colors.placeholder : colors.text,
+          }}
+        >
+          GO
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  specButtonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 16,
+  },
+  button: {
+    width: 80,
+    alignSelf: "center",
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+  },
+});
