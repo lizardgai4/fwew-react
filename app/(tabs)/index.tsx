4d9aea243de00da478c807ac3f33e3bfa819b27b
Corey Scheideman <corscheid@gmail.com>
2023-11-17 07:35:39 -0600

    [next] Result counts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,21 @@
 import { FwewSearchResults } from "@/components/search/FwewSearchResults";
 import { SearchBar } from "@/components/SearchBar";
-import { View } from "@/components/Themed";
+import { Text, View } from "@/components/Themed";
 import { useFwew } from "@/hooks/useFwew";
 import { ScrollView, StyleSheet } from "react-native";
 
 export default function TabOneScreen() {
-  const [query, results, search] = useFwew();
+  const [query, results, resultCount, search] = useFwew();
 
   return (
     <View style={styles.container}>
       <SearchBar query={query} search={search} autoFocus />
       <ScrollView style={styles.results}>
+        {query.length > 0 && resultCount > 0 && (
+          <Text style={styles.resultCount}>
+            {`${resultCount} result${resultCount === 1 ? "" : "s"}`}
+          </Text>
+        )}
         <FwewSearchResults results={results} />
       </ScrollView>
     </View>
@@ -22,6 +27,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  resultCount: {
+    padding: 16,
+    alignSelf: "center",
   },
   results: {
     width: "100%",
