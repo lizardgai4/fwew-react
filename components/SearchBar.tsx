253e44f3a4d386b39f3016e5db0ddceee96e8030
Corey Scheideman <corscheid@gmail.com>
2023-11-17 01:48:49 -0600

    Refactor SearchBar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,83 @@
+import { TextInput, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, TouchableOpacity, useColorScheme } from "react-native";
+
+interface SearchBarProps {
+  query: string;
+  search: (query: string) => void;
+  placeholder?: string;
+}
+
+export function SearchBar({
+  query,
+  search,
+  placeholder = "Search",
+}: SearchBarProps) {
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? "light"];
+
+  return (
+    <View style={styles.searchContainer}>
+      <TextInput
+        style={[styles.input, { borderColor: colors.text }]}
+        placeholder={placeholder}
+        placeholderTextColor={colors.placeholder}
+        value={query}
+        onChangeText={search}
+        autoCapitalize="none"
+        autoCorrect={false}
+        clearButtonMode="never"
+        autoFocus
+      />
+      <SearchBarRight showClear={query.length > 0} clear={() => search("")} />
+    </View>
+  );
+}
+
+interface SearchBarRightProps {
+  showClear: boolean;
+  clear: () => void;
+}
+
+function SearchBarRight({ showClear, clear }: SearchBarRightProps) {
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? "light"];
+
+  if (showClear) {
+    return (
+      <TouchableOpacity
+        style={[styles.button, { borderColor: colors.text }]}
+        onPress={clear}
+      >
+        <FontAwesome name="close" size={24} color={colors.text} />
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={[styles.button, { borderColor: colors.text }]}>
+      <FontAwesome name="search" size={24} color={colors.text} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    padding: 8,
+    borderWidth: 1,
+  },
+});
