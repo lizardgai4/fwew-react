df1019151fd14aca92803008bc43b1b56a8c9336
Corey Scheideman <corscheid@gmail.com>
2023-12-01 04:54:56 -0600

    [next] name single v1

---
+++ hooks/useNameSingle.ts
@@ -0,0 +1,56 @@
+import { useDebounce } from "@/hooks/useDebounce";
+import { Dialect, nameSingle } from "fwew.js";
+import { useEffect, useState } from "react";
+
+type NumericString = `${number}`;
+
+export default function useNameSingle() {
+  const [numNames, setNumNames] = useState<NumericString | undefined>();
+  const [numSyllables, setNumSyllables] = useState<NumericString | undefined>();
+  const [dialect, setDialect] = useState<Dialect>("forest");
+  const [names, setNames] = useState<string[]>([]);
+  const debounce = useDebounce();
+
+  const execute = async () => {
+    if (!numNames || !numSyllables || !dialect) return;
+    const names = await nameSingle(numNames, numSyllables, dialect);
+    setNames(names.trim().split("\n"));
+  };
+
+  const updateNumNames = (text: string) => {
+    if (text === "") {
+      setNumNames(undefined);
+      setNames([]);
+      return;
+    }
+    const num = parseInt(text);
+    if (isNaN(num) || num > 50 || num < 1) return;
+    setNumNames(`${num}`);
+  };
+
+  const updateNumSyllables = (text: string) => {
+    if (text === "") {
+      setNumSyllables(undefined);
+      setNames([]);
+      return;
+    }
+    const num = parseInt(text);
+    if (isNaN(num) || num > 4 || num < 1) return;
+    setNumSyllables(`${num}`);
+  };
+
+  useEffect(() => {
+    debounce(execute, 300);
+  }, [numNames, numSyllables, dialect]);
+
+  return {
+    names,
+    numNames,
+    updateNumNames,
+    numSyllables,
+    updateNumSyllables,
+    dialect,
+    setDialect,
+    execute,
+  };
+}
