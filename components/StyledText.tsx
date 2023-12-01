1b3b7a016266cc124f9c8db0306352625c6762e4
Corey Scheideman <corscheid@gmail.com>
2023-11-30 21:30:52 -0600

    [next] result info v1

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -3,3 +3,16 @@ import { Text, TextProps } from "@/components/Themed";
 export function MonoText(props: TextProps) {
   return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
 }
+
+export function BoldText(props: TextProps) {
+  return <Text {...props} style={[props.style, { fontWeight: "bold" }]} />;
+}
+
+export function UnderlinedText(props: TextProps) {
+  return (
+    <Text
+      {...props}
+      style={[props.style, { textDecorationLine: "underline" }]}
+    />
+  );
+}
