88f43fb25a4c54f7c2451e590df0550c03013920
Corey Scheideman <corscheid@gmail.com>
2023-12-02 04:20:18 -0600

    [next] simplify name single

--- components/names/NameSingle.tsx
+++ components/names/NameSingle.tsx
@@ -1,14 +1,15 @@
-import { Text, TextInput, View } from "@/components/Themed";
+import { Text, View } from "@/components/Themed";
 import Colors from "@/constants/Colors";
 import useNameSingle from "@/hooks/useNameSingle";
 import { FontAwesome } from "@expo/vector-icons";
 import {
   ScrollView,
   StyleSheet,
-  Switch,
   TouchableOpacity,
   useColorScheme,
 } from "react-native";
+import { NumericTextInput } from "../NumericTextInput";
+import { SwitchInput } from "../SwitchInput";
 
 export function NameSingle() {
   const colorScheme = useColorScheme();
@@ -27,61 +28,25 @@ export function NameSingle() {
   const disabled = !numNames || !numSyllables;
 
   return (
-    <View>
-      {/* title */}
+    <View style={styles.container}>
       <Text style={styles.title}>Single</Text>
-      {/* n input */}
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Number of names to generate (1-50)"
-          placeholderTextColor={colors.placeholder}
-          value={numNames}
-          onChangeText={updateNumNames}
-          keyboardType="number-pad"
-          style={[styles.input, { borderColor: colors.text }]}
-          autoCapitalize="none"
-          autoCorrect={false}
-          clearButtonMode="never"
-          autoFocus
-        />
-        <TouchableOpacity
-          onPress={() => updateNumNames("")}
-          style={[styles.clearButton, { borderColor: colors.text }]}
-        >
-          <FontAwesome name="close" size={24} color={colors.text} />
-        </TouchableOpacity>
-      </View>
-      {/* s input */}
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Number of syllables in the name (1-4)"
-          placeholderTextColor={colors.placeholder}
-          value={numSyllables}
-          onChangeText={updateNumSyllables}
-          keyboardType="number-pad"
-          style={[styles.input, { borderColor: colors.text }]}
-          autoCapitalize="none"
-          autoCorrect={false}
-          clearButtonMode="never"
-          autoFocus
-        />
-        <TouchableOpacity
-          onPress={() => updateNumSyllables("")}
-          style={[styles.clearButton, { borderColor: colors.text }]}
-        >
-          <FontAwesome name="close" size={24} color={colors.text} />
-        </TouchableOpacity>
-      </View>
-      {/* dialect switch */}
-      <View style={styles.switch}>
-        <Text>Forest</Text>
-        <Switch
-          value={dialect === "reef"}
-          onValueChange={(isReef) => setDialect(isReef ? "reef" : "forest")}
-        />
-        <Text>Reef</Text>
-      </View>
-      {/* generate button */}
+      <NumericTextInput
+        placeholder="Number of names to generate (1-50)"
+        value={numNames}
+        onChangeText={updateNumNames}
+        autoFocus
+      />
+      <NumericTextInput
+        placeholder="Number of syllables in the name (1-4)"
+        value={numSyllables}
+        onChangeText={updateNumSyllables}
+      />
+      <SwitchInput
+        leftLabel="Forest"
+        rightLabel="Reef"
+        value={dialect === "reef"}
+        onValueChange={(isReef) => setDialect(isReef ? "reef" : "forest")}
+      />
       <TouchableOpacity
         onPress={execute}
         disabled={disabled}
@@ -105,34 +70,14 @@ export function NameSingle() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
     padding: 10,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 8,
-  },
-  input: {
-    flex: 1,
-    padding: 10,
-    fontSize: 16,
-    borderWidth: 1,
-  },
-  clearButton: {
-    borderWidth: 1,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-  },
-  switch: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 8,
   },
   generateButton: {
     borderWidth: 1,
