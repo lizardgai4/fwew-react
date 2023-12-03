963c1a653d9998b686c5fcddc367c3b06cbc8228
Corey Scheideman <corscheid@gmail.com>
2023-12-03 07:03:27 -0600

    [next] settings
    - flag icons
    - strings
    - UI language selector component
    TODO: effect & persistence

---
+++ constants/ui/settings.ts
@@ -0,0 +1,41 @@
+import type { LanguageCode } from "fwew.js";
+
+export type ExtendedLanguageCode = LanguageCode | "es" | "nx";
+
+export type LanguageMeta = {
+  value: ExtendedLanguageCode;
+  label: string;
+  ui: boolean;
+  results: boolean;
+};
+
+export const Languages: LanguageMeta[] = [
+  // UI and Results
+  { value: "de", label: "Deutsch", ui: true, results: true },
+  { value: "en", label: "English", ui: true, results: true },
+  { value: "nl", label: "Nederlands", ui: true, results: true },
+  { value: "tr", label: "Türkçe", ui: true, results: true },
+  // UI only
+  { value: "es", label: "Español", ui: true, results: false },
+  { value: "nx", label: "Lì'fya leNa'vi", ui: true, results: false },
+  // Results only
+  { value: "et", label: "Eesti", ui: false, results: true },
+  { value: "fr", label: "Français", ui: false, results: true },
+  { value: "hu", label: "Magyar", ui: false, results: true },
+  { value: "pl", label: "Polski", ui: false, results: true },
+  { value: "ru", label: "Русский", ui: false, results: true },
+  { value: "sv", label: "Svenska", ui: false, results: true },
+];
+Languages.sort((a, b) => a.label.localeCompare(b.label));
+
+export const UILanguages = Languages.filter((l) => l.ui);
+export const ResultsLanguages = Languages.filter((l) => l.results);
+
+const strings = {
+  en: {
+    uiLanguage: "UI Language",
+    resultsLanguage: "Results Language",
+  },
+};
+
+export default strings;
