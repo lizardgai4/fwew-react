4697133a66d44096807f9480da02385036e03926
Corey Scheideman <corscheid@gmail.com>
2023-12-03 17:20:35 -0600

    [next] web version of git info

--- hooks/useVersion.ts
+++ hooks/useVersion.ts
@@ -2,13 +2,20 @@ import type { Version } from "fwew.js";
 import { version as fwewVersion } from "fwew.js";
 import { useEffect, useState } from "react";
 import Constants from "expo-constants";
+import { Platform } from "react-native";
 
 export function useVersion() {
   const [version, setVersion] = useState<Version>();
 
   const AppVersion = Constants.expoConfig?.version;
-  const CommitHash = process.env.EXPO_PUBLIC_GIT_COMMIT_HASH;
-  const Branch = process.env.EXPO_PUBLIC_GIT_BRANCH;
+  const CommitHash =
+    Platform.OS === "web"
+      ? process.env.VERCEL_GIT_COMMIT_REF
+      : process.env.EXPO_PUBLIC_GIT_COMMIT_HASH;
+  const Branch =
+    Platform.OS === "web"
+      ? process.env.VERCEL_GIT_COMMIT_SHA
+      : process.env.EXPO_PUBLIC_GIT_BRANCH;
 
   const getVersion = async () => {
     const data = await fwewVersion();
