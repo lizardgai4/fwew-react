d0dd28c45c766bdff0afc21571fb38794e5276f3
Corey Scheideman <corscheid@gmail.com>
2023-12-03 20:41:16 -0600

    [next] useResultsLanguage, extract search strings

--- components/settings/ResultsLanguageSelect.tsx
+++ components/settings/ResultsLanguageSelect.tsx
@@ -2,14 +2,12 @@ import { Accordion } from "@/components/Accordion";
 import { OptionItem } from "@/components/OptionItem";
 import { Text, View } from "@/components/Themed";
 import { FlagMap } from "@/components/settings/Flags";
-import type { ExtendedLanguageCode } from "@/constants/ui/settings";
 import strings, { ResultsLanguages } from "@/constants/ui/settings";
-import { useState } from "react";
+import { useResultsLanguage } from "@/hooks/useResultsLanguage";
 import { StyleSheet } from "react-native";
 
 export function ResultsLanguageSelect() {
-  const [resultsLanguage, setResultsLanguage] =
-    useState<ExtendedLanguageCode>("en");
+  const { resultsLanguage, saveResultsLanguage } = useResultsLanguage();
 
   return (
     <Accordion
@@ -27,7 +25,7 @@ export function ResultsLanguageSelect() {
               icon={FlagMap[language.value]}
               value={language.label}
               selected={resultsLanguage === language.value}
-              onSelect={() => setResultsLanguage(language.value)}
+              onSelect={() => saveResultsLanguage(language.value)}
             />
           ))}
         </>
