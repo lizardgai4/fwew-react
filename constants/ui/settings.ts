f893ccda1033ed0a712a3087fd58e4694d209c13
Corey Scheideman <corscheid@gmail.com>
2023-12-05 03:19:06 -0600

    [next] fix Languages array

--- constants/ui/settings.ts
+++ constants/ui/settings.ts
@@ -8,24 +8,21 @@ export type LanguageMeta = {
 };
 
 export const Languages: LanguageMeta[] = [
-  // UI and Results
   { value: "de", label: "Deutsch", ui: true, results: true },
-  { value: "en", label: "English", ui: true, results: true },
-  { value: "nl", label: "Nederlands", ui: true, results: true },
-  { value: "tr", label: "Türkçe", ui: true, results: true },
   { value: "et", label: "Eesti", ui: true, results: true },
+  { value: "en", label: "English", ui: true, results: true },
+  { value: "es", label: "Español", ui: true, results: false },
+  { value: "eo", label: "Esperanto", ui: true, results: false },
   { value: "fr", label: "Français", ui: true, results: true },
+  { value: "nx", label: "Lì'fya leNa'vi", ui: true, results: false },
   { value: "hu", label: "Magyar", ui: true, results: true },
+  { value: "nl", label: "Nederlands", ui: true, results: true },
   { value: "pl", label: "Polski", ui: true, results: true },
-  { value: "pt", label: "Português", ui: true, results: true },
+  { value: "pt", label: "Português", ui: true, results: false },
   { value: "ru", label: "Русский", ui: true, results: true },
   { value: "sv", label: "Svenska", ui: true, results: true },
-  // UI only
-  { value: "eo", label: "Esperanto", ui: true, results: false },
-  { value: "es", label: "Español", ui: true, results: false },
-  { value: "nx", label: "Lì'fya leNa'vi", ui: true, results: false },
+  { value: "tr", label: "Türkçe", ui: true, results: true },
 ];
-Languages.sort((a, b) => a.label.localeCompare(b.label));
 
 export const AppLanguages = Languages.filter((l) => l.ui);
 export const ResultsLanguages = Languages.filter((l) => l.results);
