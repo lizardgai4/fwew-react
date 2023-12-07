7098c34c6400715b50aa377902b308102fd20dc3
Corey Scheideman <corscheid@gmail.com>
2023-12-07 02:22:14 -0600

    [next] refactor: extract SelectHandler type

--- types/common.ts
+++ types/common.ts
@@ -3,3 +3,5 @@ import type { LanguageCode } from "fwew.js";
 export type ExtendedLanguageCode = LanguageCode | "eo" | "es" | "nx" | "pt";
 
 export type NumericString = `${number}` | "";
+
+export type SelectHandler = React.Dispatch<React.SetStateAction<string>>;
