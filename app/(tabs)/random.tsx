1bebd644cfa543aa6330fc62fb08814115863eaa
Corey Scheideman <corscheid@gmail.com>
2023-12-08 22:11:23 -0600

    [next] add support for multiple filters on random

--- app/(tabs)/random.tsx
+++ app/(tabs)/random.tsx
@@ -5,22 +5,35 @@ import { RefreshButton } from "@/components/common/RefreshButton";
 import { ResultCount } from "@/components/common/ResultCount";
 import { Text } from "@/components/common/Themed";
 import { ListResults } from "@/components/list/ListResults";
+import Colors from "@/constants/Colors";
 import stringsRandom from "@/constants/ui/random";
+import stringsList from "@/constants/ui/list";
 import { useAppLanguageContext } from "@/context/AppLanguageContext";
 import { useDebounce } from "@/hooks/useDebounce";
+import { FontAwesome } from "@expo/vector-icons";
 import type { Word } from "fwew.js";
 import { random } from "fwew.js";
 import { useEffect, useState } from "react";
-import { RefreshControl, ScrollView, StyleSheet } from "react-native";
+import {
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  useColorScheme,
+} from "react-native";
 
 export default function RandomScreen() {
   const [numWords, setNumWords] = useState("8");
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState<Word[]>([]);
-  const [filterExpression, setFilterExpression] = useState("");
+  const [filterExpressions, setFilterExpressions] = useState<string[]>([""]);
   const debounce = useDebounce();
   const { appLanguage } = useAppLanguageContext();
-  const ui = stringsRandom[appLanguage];
+  const uiRandom = stringsRandom[appLanguage];
+  const uiList = stringsList[appLanguage];
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? "light"];
+  const addDisabled = filterExpressions[filterExpressions.length - 1] === "";
 
   const updateNumWords = (num: string) => {
     if (num === "") {
@@ -34,13 +47,23 @@ export default function RandomScreen() {
     setNumWords(num);
   };
 
+  const addFilterExpression = () => {
+    setFilterExpressions([...filterExpressions, ""]);
+  };
+
+  const updateFilterExpression = (index: number, expression: string) => {
+    const newExpressions = [...filterExpressions];
+    newExpressions[index] = expression;
+    setFilterExpressions(newExpressions);
+  };
+
   const execute = async () => {
     if (numWords.length === 0) {
       return;
     }
     setLoading(true);
-    if (filterExpression.length > 0) {
-      const data = await random(+numWords, filterExpression);
+    if (filterExpressions.join("").length > 0) {
+      const data = await random(+numWords, filterExpressions.join(" and "));
       setResults(data);
       setLoading(false);
       return;
@@ -59,7 +82,7 @@ export default function RandomScreen() {
       debounce(execute);
       return;
     }
-  }, [numWords, filterExpression]);
+  }, [numWords, filterExpressions]);
 
   return (
     <ScrollView
@@ -72,17 +95,44 @@ export default function RandomScreen() {
       }
     >
       <Accordion
-        closedContent={<Text>{ui.randomOptions}</Text>}
+        closedContent={<Text>{uiRandom.randomOptions}</Text>}
         openedContent={
           <>
-            <Text style={styles.label}>{ui.numWords}</Text>
+            <Text style={styles.label}>{uiRandom.numWords}</Text>
             <NumericTextInput
               placeholder={"1-100"}
               onChangeText={updateNumWords}
               value={numWords}
             />
-            <Text style={styles.label}>{ui.where}</Text>
-            <FilterExpressionBuilder onChange={setFilterExpression} />
+            <Text style={styles.label}>{uiRandom.where}</Text>
+            {filterExpressions.map((_, i) => (
+              <>
+                {i > 0 && <Text style={styles.label}>{uiList.and}</Text>}
+                <FilterExpressionBuilder
+                  key={`feb_${i}`}
+                  onChange={(text) => updateFilterExpression(i, text)}
+                />
+              </>
+            ))}
+            <TouchableOpacity
+              onPress={addFilterExpression}
+              style={{
+                flexDirection: "row",
+                gap: 8,
+                padding: 16,
+                borderWidth: 1,
+                borderColor: addDisabled ? colors.placeholder : colors.text,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              disabled={addDisabled}
+            >
+              <FontAwesome
+                name="plus"
+                size={24}
+                color={addDisabled ? colors.placeholder : colors.text}
+              />
+            </TouchableOpacity>
           </>
         }
       />
