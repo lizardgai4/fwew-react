0696c7342a7cc8d6618ff1d012387431b9d026f8
Corey Scheideman <corscheid@gmail.com>
2023-12-09 11:33:23 -0600

    [next] fix filter expression spec inputs

---
+++ components/common/AlphaTextInput.tsx
@@ -0,0 +1,62 @@
+import { TextInput, View } from "@/components/common/Themed";
+import Colors from "@/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, TouchableOpacity, useColorScheme } from "react-native";
+
+type AlphaTextInputProps = {
+  value?: string | `${number}` | undefined;
+  onChangeText: (text: string) => void;
+  placeholder: string;
+  autoFocus?: boolean;
+};
+
+export function AlphaTextInput({
+  value,
+  onChangeText,
+  placeholder,
+  autoFocus,
+}: AlphaTextInputProps) {
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? "light"];
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor={colors.placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        keyboardType="default"
+        style={[styles.input, { borderColor: colors.text }]}
+        autoCapitalize="none"
+        autoCorrect={false}
+        clearButtonMode="never"
+        autoFocus={autoFocus}
+      />
+      <TouchableOpacity
+        onPress={() => onChangeText("")}
+        style={[styles.clearButton, { borderColor: colors.text }]}
+      >
+        <FontAwesome name="close" size={24} color={colors.text} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    fontSize: 16,
+    borderWidth: 1,
+  },
+  clearButton: {
+    borderWidth: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+  },
+});
