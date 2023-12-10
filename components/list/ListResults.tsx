cfad20d872976164b67f023a9a2044f038f297f4
Corey Scheideman <corscheid@gmail.com>
2023-12-09 19:07:31 -0600

    [next] only show list result spinner on web; mobile has refreshcontrol

--- components/list/ListResults.tsx
+++ components/list/ListResults.tsx
@@ -3,7 +3,7 @@ import { Text } from "@/components/common/Themed";
 import strings from "@/constants/ui/list";
 import { useAppLanguageContext } from "@/context/AppLanguageContext";
 import type { Word } from "fwew.js";
-import { ActivityIndicator, StyleSheet } from "react-native";
+import { ActivityIndicator, Platform, StyleSheet } from "react-native";
 
 type ListResultsProps = {
   loading?: boolean;
@@ -13,7 +13,7 @@ type ListResultsProps = {
 export function ListResults({ loading, results }: ListResultsProps) {
   const { appLanguage } = useAppLanguageContext();
   const ui = strings[appLanguage];
-  if (loading) {
+  if (loading && Platform.OS === "web") {
     return <ActivityIndicator size="large" />;
   }
   if (results === undefined || results.map === undefined) {
