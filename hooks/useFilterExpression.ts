ff7254bde99a13818ff70a28092cdf2d419c6b55
Corey Scheideman <corscheid@gmail.com>
2023-12-09 22:31:16 -0600

    [next] refactor random, extract useFilterExpression hook

---
+++ hooks/useFilterExpression.ts
@@ -0,0 +1,80 @@
+import strings from "@/constants/ui/list";
+import { useAppLanguageContext } from "@/context/AppLanguageContext";
+import { FilterExpressionBuilderValue } from "@/types/list";
+import { useEffect, useState } from "react";
+
+export function useFilterExpression() {
+  const [filters, setFilters] = useState<FilterExpressionBuilderValue[]>([
+    { spec: "" },
+  ]);
+
+  const { appLanguage } = useAppLanguageContext();
+  const ui = strings[appLanguage];
+
+  const disabled = filters[filters.length - 1]?.spec === "";
+
+  const incomplete =
+    filters.filter((fe) => (fe.what || fe.cond) && !fe.spec.trim()).length > 0;
+
+  const filterExpression = filters
+    .map((f) =>
+      `${f.what?.value ?? ""} ${f.cond?.value ?? ""} ${f.spec}`
+        .trim()
+        .replace(/ +/g, " ")
+    )
+    .join(" and ")
+    .trim();
+
+  const addFilterExpression = () => {
+    setFilters([...filters, { spec: "" }]);
+  };
+
+  const removeFilterExpression = (index: number) => {
+    const newExpressions = [...filters];
+    newExpressions.splice(index, 1);
+    setFilters(newExpressions);
+  };
+
+  const updateFilterExpression = (
+    index: number,
+    expression: FilterExpressionBuilderValue
+  ) => {
+    const newExpressions = [...filters];
+    newExpressions[index] = expression;
+    setFilters(newExpressions);
+  };
+
+  useEffect(() => {
+    setFilters((prev) => {
+      return prev.map((e) => {
+        return {
+          what: e.what && {
+            value: e.what.value,
+            description: ui.listMenu.whatValues.filter(
+              (w) => w.value === e.what?.value
+            )[0]?.description,
+          },
+          cond: e.cond && {
+            value: e.cond.value,
+            description:
+              e.what &&
+              ui.listMenu.condValues[e.what.value].filter(
+                (c) => c.value === e.cond?.value
+              )[0]?.description,
+          },
+          spec: e.spec,
+        } as FilterExpressionBuilderValue;
+      });
+    });
+  }, [appLanguage]);
+
+  return {
+    filters,
+    filterExpression,
+    disabled,
+    incomplete,
+    addFilterExpression,
+    removeFilterExpression,
+    updateFilterExpression,
+  };
+}
