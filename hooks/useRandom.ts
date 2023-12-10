6628fe41444e84a12dadcc7bac67e2d0c3aa294f
Corey Scheideman <corscheid@gmail.com>
2023-12-10 01:07:23 -0600

    [next] extract useRandom hook, update numeric input types

---
+++ hooks/useRandom.ts
@@ -0,0 +1,27 @@
+import { NumericString } from "@/types/common";
+import { random, type Word } from "fwew.js";
+import { useState } from "react";
+
+export function useRandom() {
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState<Word[]>([]);
+
+  const execute = async (numWords: NumericString, filterExpression: string) => {
+    if (numWords.length === 0) {
+      setResults([]);
+      return;
+    }
+    setLoading(true);
+    if (filterExpression.length > 0) {
+      const data = await random(+numWords, filterExpression);
+      setResults(data);
+      setLoading(false);
+      return;
+    }
+    const data = await random(+numWords);
+    setResults(data);
+    setLoading(false);
+  };
+
+  return { loading, results, execute };
+}
