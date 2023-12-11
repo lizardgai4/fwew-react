03d5bb857d7f4ccc2302a35407e3d899f4c0f328
Corey Scheideman <corscheid@gmail.com>
2023-12-10 18:27:06 -0600

    [next] refactor list, reuse more components, remove unused code

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,53 +1,56 @@
 import { ResultCount } from "@/components/common/ResultCount";
-import { SearchBar } from "@/components/common/SearchBar";
-import { View } from "@/components/common/Themed";
 import { ListOptions } from "@/components/list/ListOptions";
 import { ListResults } from "@/components/list/ListResults";
-import strings from "@/constants/ui/list";
-import { useAppLanguageContext } from "@/context/AppLanguageContext";
+import { useDebounce } from "@/hooks/useDebounce";
+import { useFilterExpression } from "@/hooks/useFilterExpression";
 import { useList } from "@/hooks/useList";
+import { useEffect } from "react";
 import { RefreshControl, ScrollView, StyleSheet } from "react-native";
 
 export default function ListScreen() {
-  const { query, results, loading, updateQuery, execute } = useList();
-  const { appLanguage } = useAppLanguageContext();
-  const ui = strings[appLanguage];
+  const { filters, filterExpression, incomplete, add, remove, update } =
+    useFilterExpression();
+  const { loading, results, execute } = useList();
+  const debounce = useDebounce();
+  const resultsVisible = filterExpression.length > 0 && results.length > 0;
+
+  const getData = async () => {
+    if (filterExpression.length === 0) {
+      return;
+    }
+    if (filterExpression.length > 0 && incomplete) {
+      return;
+    }
+    debounce(() => execute(filterExpression));
+  };
+
+  useEffect(() => {
+    if (!incomplete) {
+      getData();
+    }
+  }, [filterExpression]);
 
   return (
-    <View style={styles.container}>
-      <SearchBar
-        query={query}
-        search={updateQuery}
-        placeholder={ui.list}
-        execute={execute}
-        autoFocus
+    <ScrollView
+      keyboardShouldPersistTaps="always"
+      refreshControl={
+        <RefreshControl refreshing={loading} onRefresh={getData} />
+      }
+    >
+      <ListOptions
+        filters={filters}
+        add={add}
+        remove={remove}
+        update={update}
+        incomplete={incomplete}
       />
-      <ScrollView
-        keyboardShouldPersistTaps="always"
-        refreshControl={
-          <RefreshControl refreshing={loading} onRefresh={execute} />
-        }
-      >
-        <ListOptions
-          title={ui.listOptions}
-          query={query}
-          onSelect={updateQuery}
-          execute={execute}
-        />
-        <ResultCount
-          visible={query.length > 0 && results.length > 0}
-          resultCount={results.length}
-        />
-        <ListResults loading={loading} results={results} />
-      </ScrollView>
-    </View>
+      <ResultCount visible={resultsVisible} resultCount={results.length} />
+      <ListResults loading={loading} results={resultsVisible ? results : []} />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   resultCount: {
     padding: 16,
     alignSelf: "center",
--- components/list/ListOptions.tsx
+++ components/list/ListOptions.tsx
@@ -1,70 +1,41 @@
-import { BackButton } from "@/components/common/BackButton";
-import { Text, View } from "@/components/common/Themed";
-import { useListOptions } from "@/hooks/useListOptions";
-import { useEffect } from "react";
-import { StyleSheet } from "react-native";
-import { ListOptionsForm } from "./ListOptionsForm";
+import { Accordion } from "@/components/common/Accordion";
+import { FilterExpressionBuilderList } from "@/components/common/FilterExpressionBuilderList";
+import { Text } from "@/components/common/Themed";
+import strings from "@/constants/ui/list";
+import { useAppLanguageContext } from "@/context/AppLanguageContext";
+import type { FilterExpressionBuilderValue } from "@/types/list";
 
 type ListOptionsProps = {
-  title: string;
-  query: string;
-  onSelect: React.Dispatch<React.SetStateAction<string>>;
-  execute: () => void;
+  filters: FilterExpressionBuilderValue[];
+  add: () => void;
+  remove: (index: number) => void;
+  update: (index: number, filter: FilterExpressionBuilderValue) => void;
+  incomplete: boolean;
 };
 
 export function ListOptions({
-  title,
-  query,
-  onSelect,
-  execute,
+  filters,
+  add,
+  remove,
+  update,
+  incomplete,
 }: ListOptionsProps) {
-  const { whatRef, mode, reset, prevMode, selectOption } =
-    useListOptions(onSelect);
-
-  const backButtonDisabled = mode === "what";
-
-  const andButtonDisabled =
-    query.length === 0 ||
-    query.trim().split(" ").length < 3 ||
-    (query.trim().split(" ").length + 1) % 4 !== 0 ||
-    query.trim().endsWith("and");
-
-  useEffect(() => {
-    if (query === "") {
-      reset();
-      return;
-    }
-    if (/\s\s+/g.test(query)) {
-      onSelect((prev) => prev.replace(/\s\s+/g, " "));
-    }
-  }, [query]);
-
+  const { appLanguage } = useAppLanguageContext();
+  const ui = strings[appLanguage];
   return (
-    <>
-      <View style={styles.titleContainer}>
-        <BackButton onPress={prevMode} disabled={backButtonDisabled} />
-        <Text style={styles.title}>{title}</Text>
-      </View>
-      <ListOptionsForm
-        mode={mode}
-        what={whatRef.current}
-        selectOption={selectOption}
-        execute={execute}
-        andButtonDisabled={andButtonDisabled}
-      />
-    </>
+    <Accordion
+      closedContent={<Text>{ui.listOptions}</Text>}
+      openedContent={
+        <>
+          <FilterExpressionBuilderList
+            filters={filters}
+            add={add}
+            remove={remove}
+            update={update}
+            disabled={incomplete}
+          />
+        </>
+      }
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-    paddingTop: 16,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
