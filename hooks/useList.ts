ef47a1a31b9c81e4300e5581f3f472167eb31163
Corey Scheideman <corscheid@gmail.com>
2023-12-10 18:27:06 -0600

    [next] refactor list, reuse more components, remove unused code

--- hooks/useList.ts
+++ hooks/useList.ts
@@ -1,32 +1,19 @@
 import type { Word } from "fwew.js";
 import { list } from "fwew.js";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function useList() {
-  const [query, updateQuery] = useState("");
   const [results, setResults] = useState<Word[]>([]);
   const [loading, setLoading] = useState(false);
 
-  const args = query.trim().split(" ");
-
-  const execute = async () => {
-    setLoading(true);
-    if (args.length === 0) {
-      setResults([]);
+  const execute = async (filterExpression: string) => {
+    if (filterExpression.length > 0) {
+      setLoading(true);
+      const data = await list(filterExpression);
+      setResults(data);
       setLoading(false);
-      return;
     }
-    const data = await list(query);
-    setResults(data);
-    setLoading(false);
   };
 
-  useEffect(() => {
-    if (query === "" && results.length > 0) {
-      setResults([]);
-      return;
-    }
-  }, [query]);
-
-  return { query, results, loading, updateQuery, execute } as const;
+  return { loading, results, execute };
 }
