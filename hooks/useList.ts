4ffeda788468387a2b84e593c50449ab31db0ed2
Corey Scheideman <corscheid@gmail.com>
2023-12-10 21:21:19 -0600

    [next] useCallback

--- hooks/useList.ts
+++ hooks/useList.ts
@@ -1,19 +1,18 @@
 import type { Word } from "fwew.js";
 import { list } from "fwew.js";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useList() {
   const [results, setResults] = useState<Word[]>([]);
   const [loading, setLoading] = useState(false);
 
-  const execute = async (filterExpression: string) => {
-    if (filterExpression.length > 0) {
-      setLoading(true);
-      const data = await list(filterExpression);
-      setResults(data);
-      setLoading(false);
-    }
-  };
+  const execute = useCallback(async (filterExpression: string) => {
+    if (filterExpression.length === 0) return;
+    setLoading(true);
+    const data = await list(filterExpression);
+    setResults(data);
+    setLoading(false);
+  }, []);
 
   return { loading, results, execute };
 }
--- hooks/useRandom.ts
+++ hooks/useRandom.ts
@@ -1,27 +1,30 @@
 import { NumericString } from "@/types/common";
 import { random, type Word } from "fwew.js";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useRandom() {
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState<Word[]>([]);
 
-  const execute = async (numWords: NumericString, filterExpression: string) => {
-    if (numWords.length === 0) {
-      setResults([]);
-      return;
-    }
-    setLoading(true);
-    if (filterExpression.length > 0) {
-      const data = await random(+numWords, filterExpression);
+  const execute = useCallback(
+    async (numWords: NumericString, filterExpression: string) => {
+      if (numWords.length === 0) {
+        setResults([]);
+        return;
+      }
+      setLoading(true);
+      if (filterExpression.length > 0) {
+        const data = await random(+numWords, filterExpression);
+        setResults(data);
+        setLoading(false);
+        return;
+      }
+      const data = await random(+numWords);
       setResults(data);
       setLoading(false);
-      return;
-    }
-    const data = await random(+numWords);
-    setResults(data);
-    setLoading(false);
-  };
+    },
+    []
+  );
 
   return { loading, results, execute };
 }
