23d4f6033e95991b9a99d5aa21a5ec66e563e8b8
Corey Scheideman <corscheid@gmail.com>
2023-12-19 18:32:05 -0600

    [next] Random styles v1

--- components/list/ListOptions.tsx
+++ components/list/ListOptions.tsx
@@ -30,6 +30,7 @@ export function ListOptions(props: ListOptionsProps) {
             remove={remove}
             update={update}
             disabled={incomplete}
+            mode="list"
           />
         </>
       }
