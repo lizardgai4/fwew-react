4348be2f3e445e98377209a1a94de7dc6d75d166
Corey Scheideman <corscheid@gmail.com>
2023-12-21 17:47:07 -0600

    [next] fix button and option item colors

--- components/common/OptionItem.tsx
+++ components/common/OptionItem.tsx
@@ -1,4 +1,5 @@
 import { CardView, Text } from "@/components/common/Themed";
+import Colors from "@/constants/Colors";
 import { FontAwesome } from "@expo/vector-icons";
 import { useTheme } from "@react-navigation/native";
 import { StyleSheet, TouchableOpacity } from "react-native";
@@ -12,23 +13,33 @@ type OptionItemProps = {
 
 export function OptionItem(props: OptionItemProps) {
   const { icon, value, selected, onSelect } = props;
-  const { colors } = useTheme();
+  const theme = useTheme();
+
+  const getTextColor = () => {
+    if (selected) return Colors.dark.text;
+    if (theme.dark) return Colors.dark.placeholder;
+    return Colors.light.text;
+  };
 
   return (
     <TouchableOpacity onPress={onSelect} style={styles.container}>
       <CardView
         style={[
           styles.iconContainer,
-          { backgroundColor: selected ? colors.primary : colors.card },
+          {
+            backgroundColor: selected
+              ? theme.colors.primary
+              : theme.colors.card,
+          },
         ]}
       >
         {icon}
-        <Text style={styles.value}>{value}</Text>
+        <Text style={[styles.value, { color: getTextColor() }]}>{value}</Text>
         {selected && (
           <FontAwesome
             name="check"
             size={24}
-            color={colors.text}
+            color={Colors.dark.text}
             style={styles.check}
           />
         )}
