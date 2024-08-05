5ce1e2107ee108a83258d2992b25fdeaebe33648
Corey Scheideman <corscheid@gmail.com>
2024-08-04 22:08:50 -0500

    Rewrite Lenition page

--- app/(tabs)/other/lenition.tsx
+++ app/(tabs)/other/lenition.tsx
@@ -1,80 +1,114 @@
-import { BoldText } from "@/components/common/StyledText";
+import { BoldText, MonoText } from "@/components/common/StyledText";
 import { Text, View } from "@/components/common/Themed";
+import { ListResults } from "@/components/list/ListResults";
 import i18n from "@/constants/i18n";
 import { useAppLanguageContext } from "@/context/AppLanguageContext";
-import { FlatList, StyleSheet } from "react-native";
+import { useList } from "@/hooks/useList";
+import { useEffect } from "react";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function LenitionScreen() {
+  return (
+    <ScrollView style={{ padding: 16 }}>
+      <LenitionTable />
+      <LenPreList />
+      <LenAdpList />
+    </ScrollView>
+  );
+}
+
+function LenitionTable() {
   const { appLanguage } = useAppLanguageContext();
   const ui = i18n[appLanguage].lenition;
 
+  const lenitionData = [
+    { key: "Kx", value: "⇾ K" },
+    { key: "Px", value: "⇾ P" },
+    { key: "Tx", value: "⇾ T" },
+    { key: "K ", value: "⇾ H" },
+    { key: "P ", value: "⇾ F" },
+    { key: "T ", value: "⇾ S" },
+    { key: "Ts", value: "⇾ S" },
+    { key: "' ", value: `⇾ ${ui.glottalStop}` },
+  ];
+
+  return (
+    <View>
+      {lenitionData.map(({ key, value }, i) => (
+        <View key={`lt_r_${i}`} style={styles.lenitionRow}>
+          <MonoText style={styles.lenitionKey}>{key}</MonoText>
+          <MonoText style={styles.lenitionValue}>{value}</MonoText>
+        </View>
+      ))}
+    </View>
+  );
+}
+
+function LenPreList() {
+  const { appLanguage } = useAppLanguageContext();
+  const ui = i18n[appLanguage].lenition;
+
+  const lenPrefixes = [
+    "me+",
+    "pxe+",
+    "ay+",
+    "fìme+",
+    "fìpxe+",
+    "fay+",
+    "tsame+",
+    "tsapxe+",
+    "tsay+",
+    "fray+",
+    "pe+",
+    "pay+",
+  ];
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={[
-          { key: "Kx", value: "⇾ K" },
-          { key: "Px", value: "⇾ P" },
-          { key: "Tx", value: "⇾ T" },
-          { key: "K ", value: "⇾ H" },
-          { key: "P ", value: "⇾ F" },
-          { key: "T ", value: "⇾ S" },
-          { key: "Ts", value: "⇾ S" },
-          { key: "' ", value: `⇾ ${ui.glottalStop}` },
-        ]}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.row}>
-              <BoldText style={styles.lenitionHeader}>{item.key}</BoldText>
-              <Text style={styles.lenitionValue}>{item.value}</Text>
-            </View>
-          );
-        }}
-      />
+      <BoldText style={styles.header}>{ui.lenitingPrefixes}</BoldText>
+      <Text style={styles.prefixText}>{lenPrefixes.join(", ")}</Text>
+    </View>
+  );
+}
 
-      <FlatList
-        data={[
-          { key: ui.lenitingPrefixes, value: "me+, pxe+, ay+, pe+" },
-          {
-            key: ui.lenitingAdpositions,
-            value: "fpi, ìlä, lisre, mì, nuä, pxisre, ro, sko, sre, wä",
-          },
-        ]}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.row}>
-              <BoldText style={styles.lenitingText}>{item.key}</BoldText>
-              <Text style={styles.lenitingText}>{item.value}</Text>
-            </View>
-          );
-        }}
-      />
+function LenAdpList() {
+  const { appLanguage } = useAppLanguageContext();
+  const ui = i18n[appLanguage].lenition;
+  const { loading, execute, results } = useList();
+
+  useEffect(() => {
+    void execute("pos has adp. and word ends +");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <BoldText style={styles.header}>{ui.lenitingAdpositions}</BoldText>
+      <ListResults results={results} loading={loading} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 16,
+  lenitionRow: {
+    flexDirection: "row"
   },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 16,
-  },
-  lenitionHeader: {
-    fontFamily: "monospace",
+  lenitionKey: {
     fontSize: 18,
-    paddingVertical: 8,
+    paddingVertical: 8
   },
   lenitionValue: {
-    fontFamily: "monospace",
     fontSize: 18,
-    padding: 8,
+    padding: 8
   },
-  lenitingText: {
-    fontSize: 18,
+  container: {
+    paddingVertical: 8
+  },
+  header: {
+    fontSize: 20,
     paddingVertical: 8,
+  },
+  prefixText: {
+    fontSize: 18
   },
 });
