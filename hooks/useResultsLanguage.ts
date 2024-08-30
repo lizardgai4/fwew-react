98a17cfbcfc93a8f542d7e0f9eb03b4115151108
lizardgai4 <lizardgai4@gmail.com>
2024-08-30 00:57:10 -0700

    Made reef dialect UI

--- hooks/useResultsLanguage.ts
+++ hooks/useResultsLanguage.ts
@@ -8,7 +8,7 @@ export function useResultsLanguage() {
   const { getItem, setItem } = useAsyncStorage("fw_lang_results");
 
   async function saveResultsLanguage(value: ExtendedLanguageCode) {
-    if (value === "eo" || value === "nx") {
+    if (value === "eo" || value === "nx" || value == "lw") {
       return;
     }
     try {
