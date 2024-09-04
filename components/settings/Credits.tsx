4a83347a278f542efc4450db2671a4acdecd2a78
Corey Scheideman <corscheid@gmail.com>
2024-09-03 19:37:43 -0500

    Update credits page

--- components/settings/Credits.tsx
+++ components/settings/Credits.tsx
@@ -1,10 +1,13 @@
 import { CardView, Text } from "@/components/common/Themed";
 import credits from "@/constants/Credits";
 import { getUI } from "@/constants/i18n";
+import { AppLanguages } from "@/constants/Language";
 import { useAppLanguageContext } from "@/context/AppLanguageContext";
 import { useDialectContext } from "@/context/DialectContext";
+import { ExtendedLanguageCode } from "@/types/common";
 import { useTheme } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
+import { FlagMap } from "./Flags";
 
 export function Credits() {
   const { appLanguage } = useAppLanguageContext();
@@ -13,41 +16,56 @@ export function Credits() {
   return (
     <CardView style={styles.creditsContainer}>
       <Text style={styles.label}>{ui.settings.credits}</Text>
-      <CreditsItem
-        title={ui.settings.development}
-        names={credits.development}
-      />
-      <CreditsItem title={ui.settings.design} names={credits.design} />
-      <CreditsItem title={ui.settings.testing} names={credits.testing} />
-      <CreditsItem
-        title={ui.settings.translation}
-        names={credits.translation}
-      />
+      <Text style={styles.label}>{ui.settings.development}</Text>
+      <CreditsItem names={credits.development} />
+      <Text style={styles.label}>{ui.settings.design}</Text>
+      <CreditsItem names={credits.design} />
+      <Text style={styles.label}>{ui.settings.testing}</Text>
+      <CreditsItem names={credits.testing} />
+      <Text style={styles.label}>{ui.settings.translation}</Text>
+      {AppLanguages.map((language, i) => {
+        return (
+          <LanguageCreditsItem
+            key={`sct_${i}_${language.value}`}
+            language={language.value}
+            names={credits.translation[language.value]}
+          />
+        );
+      })}
     </CardView>
   );
 }
 
-function CreditsItem({ title, names }: { title: string; names: string[] }) {
+function CreditsItem({ names }: { names: string[] }) {
   const { colors } = useTheme();
 
   return (
-    <CardView>
-      <Text style={styles.label}>{title}</Text>
-      <CardView style={styles.creditsItemContainer}>
-        {names.map((name, i) => (
-          <CardView
-            key={`ci_${title}_${i}`}
-            style={[
-              styles.textContainer,
-              { backgroundColor: colors.background },
-            ]}
-          >
-            <Text style={styles.text}>{name}</Text>
-          </CardView>
-        ))}
-      </CardView>
+    <CardView style={styles.creditsItemContainer}>
+      {names.map((name, i) => (
+        <CardView
+          key={`ci_${name}_${i}`}
+          style={[styles.textContainer, { backgroundColor: colors.background }]}
+        >
+          <Text style={styles.text}>{name}</Text>
+        </CardView>
+      ))}
     </CardView>
   );
+}
+
+function LanguageCreditsItem({
+  language,
+  names,
+}: {
+  language: ExtendedLanguageCode;
+  names: string[];
+}) {
+  if (names.length > 0)
+    return (
+      <CardView style={styles.creditsItemContainer}>
+        {FlagMap[language]} <CreditsItem names={names} />
+      </CardView>
+    );
 }
 
 const styles = StyleSheet.create({
