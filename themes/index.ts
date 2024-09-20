86fa85c8c4ebff8db45e8443e10549977fb70d6a
Corey Scheideman <corscheid@gmail.com>
2024-09-19 20:07:13 -0500

    refactor: simplify theme objects

--- themes/index.ts
+++ themes/index.ts
@@ -1,49 +1,49 @@
-import { DarkTheme, DefaultTheme, type Theme } from "@react-navigation/native";
+import { type Theme } from "@react-navigation/native";
 
 export const FwewDarkTheme: Theme = {
   dark: true,
   colors: {
-    ...DarkTheme.colors,
     primary: "#3a5575",
     background: "#282b2d",
     card: "#181a1b",
     text: "#e8e6e3",
     border: "#3b5778",
+    notification: "#ff453a",
   },
 };
 
 export const FwewDarkReefTheme: Theme = {
-  ...FwewDarkTheme,
+  dark: true,
   colors: {
-    ...FwewDarkTheme.colors,
     primary: "#3a7569",
     background: "#282b2d",
     card: "#181a1b",
     text: "#e8e6e3",
     border: "#3b786c",
+    notification: "#ff453a",
   },
 };
 
 export const FwewLightTheme: Theme = {
   dark: false,
   colors: {
-    ...DefaultTheme.colors,
     primary: "#7494ba",
     background: "#d9e2ed",
     card: "#ffffff",
     text: "#000000",
     border: "#537aa8",
+    notification: "#ff3b30",
   },
 };
 
 export const FwewLightReefTheme: Theme = {
-  ...FwewLightTheme,
+  dark: false,
   colors: {
-    ...FwewLightTheme.colors,
     primary: "#74baac",
     background: "#d9e2ed",
     card: "#ffffff",
     text: "#000000",
     border: "#53a897",
+    notification: "#ff3b30",
   },
 };
