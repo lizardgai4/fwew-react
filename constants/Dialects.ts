b0702122cd546e7d4eee457dff1e4b596eae2a4e
Corey Scheideman <corscheid@gmail.com>
2024-09-21 14:52:05 -0500

    remove as const

--- constants/Dialects.ts
+++ constants/Dialects.ts
@@ -3,9 +3,9 @@ import { DialectDisplayType, DialectMeta } from "@/types/common";
 export const Dialects: DialectMeta[] = [
   { name: "Lì'fya Na'rìngä", value: "forest", abbr: "LN" },
   { name: "Lì'fya Wione", value: "reef", abbr: "LW" },
-] as const;
+];
 
 export const DialectDisplay: DialectDisplayType = {
   forest: Dialects[0],
   reef: Dialects[1],
-} as const;
+};
