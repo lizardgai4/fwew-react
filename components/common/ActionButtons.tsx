754f90fab57a61ef2a9f721c4d1f4a035761da69
Corey Scheideman <corscheid@gmail.com>
2024-09-20 19:06:44 -0500

    refactor ActionButtons

--- components/common/ActionButtons.tsx
+++ components/common/ActionButtons.tsx
@@ -1,85 +1,73 @@
 import Colors from "@/constants/Colors";
 import { useDialectContext } from "@/context/DialectContext";
+import { FontAwesomeIconName } from "@/types/icons";
 import { FontAwesome } from "@expo/vector-icons";
-import { Link } from "expo-router";
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-  type ViewStyle,
-} from "react-native";
+import { Href, Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-type ActionButtonsProps = {
-  style?: ViewStyle;
-};
-
-export function ActionButtons({ style }: ActionButtonsProps) {
-  const { dialectDisplay, toggleDialect } = useDialectContext();
-
+export function ActionButtons() {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        padding: 16,
-        gap: 16,
-        ...style,
-      }}
-    >
-      <Pressable style={styles.actionButton} onPress={toggleDialect}>
-        {({ pressed }) => (
-          <View
-            style={{
-              height: 25,
-              justifyContent: "center",
-              alignItems: "center",
-              opacity: pressed ? 0.5 : 1,
-            }}
-          >
-            <Text
-              style={{
-                color: "#fff",
-                fontSize: 20,
-              }}
-            >
-              {dialectDisplay}
-            </Text>
-          </View>
-        )}
-      </Pressable>
-      <Link href="/favorites" asChild>
-        <Pressable style={styles.actionButton}>
-          {({ pressed }) => (
-            <FontAwesome
-              name="heart"
-              size={25}
-              color={Colors.dark.text}
-              style={{ opacity: pressed ? 0.5 : 1 }}
-            />
-          )}
-        </Pressable>
-      </Link>
-      <Link href="/settings" asChild>
-        <Pressable style={styles.actionButton}>
-          {({ pressed }) => (
-            <FontAwesome
-              name="gear"
-              size={25}
-              color={Colors.dark.text}
-              style={{ opacity: pressed ? 0.5 : 1 }}
-            />
-          )}
-        </Pressable>
-      </Link>
+    <View style={styles.container}>
+      <DialectButton />
+      <ActionButton href="/favorites" icon="heart" />
+      <ActionButton href="/settings" icon="gear" />
     </View>
   );
 }
 
+function DialectButton() {
+  const { dialectDisplay, toggleDialect } = useDialectContext();
+  return (
+    <Pressable style={styles.actionButton} onPress={toggleDialect}>
+      {({ pressed }) => (
+        <View style={[styles.dialectButton, { opacity: pressed ? 0.5 : 1 }]}>
+          <Text style={styles.dialectText}>{dialectDisplay}</Text>
+        </View>
+      )}
+    </Pressable>
+  );
+}
+
+type ABProps = {
+  href: Href<string>;
+  icon: FontAwesomeIconName;
+};
+
+function ActionButton({ href, icon }: ABProps) {
+  return (
+    <Link href={href} asChild>
+      <Pressable style={styles.actionButton}>
+        {({ pressed }) => (
+          <FontAwesome
+            name={icon}
+            size={25}
+            color={Colors.dark.text}
+            style={{ opacity: pressed ? 0.5 : 1 }}
+          />
+        )}
+      </Pressable>
+    </Link>
+  );
+}
+
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 16,
+    gap: 16,
+  },
+  dialectButton: {
+    height: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dialectText: {
+    color: "#fff",
+    fontSize: 20,
+  },
   actionButton: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    // padding: 16,
   },
 });
