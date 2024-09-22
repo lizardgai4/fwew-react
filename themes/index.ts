7842cc0edab5e42cf16423b77ee91e810a20ad3a
Corey Scheideman <corscheid@gmail.com>
2024-09-21 22:26:39 -0500

    [WIP] Refactor theme system for modularization

--- themes/index.ts
+++ themes/index.ts
@@ -1,70 +1,13 @@
 import { type Theme } from "@react-navigation/native";
 import { type ColorSchemeName } from "react-native";
 import { type Dialect } from "@/types/common";
-
-const FwewDarkTheme: Theme = {
-  dark: true,
-  colors: {
-    primary: "#3a5575",
-    background: "#282b2d",
-    card: "#181a1b",
-    text: "#e8e6e3",
-    border: "#3b5778",
-    notification: "#ff453a",
-  },
-};
-
-const FwewDarkReefTheme: Theme = {
-  dark: true,
-  colors: {
-    primary: "#3a7569",
-    background: "#282b2d",
-    card: "#181a1b",
-    text: "#e8e6e3",
-    border: "#3b786c",
-    notification: "#ff453a",
-  },
-};
-
-const FwewLightTheme: Theme = {
-  dark: false,
-  colors: {
-    primary: "#7494ba",
-    background: "#d9e2ed",
-    card: "#ffffff",
-    text: "#000000",
-    border: "#537aa8",
-    notification: "#ff3b30",
-  },
-};
-
-const FwewLightReefTheme: Theme = {
-  dark: false,
-  colors: {
-    primary: "#74baac",
-    background: "#d9e2ed",
-    card: "#ffffff",
-    text: "#000000",
-    border: "#53a897",
-    notification: "#ff3b30",
-  },
-};
+import { FwewTheme } from "@/themes/fwew";
 
 const ThemeMap = {
-  fwew: {
-    light: {
-      forest: FwewLightTheme,
-      reef: FwewLightReefTheme,
-    },
-    dark: {
-      forest: FwewDarkTheme,
-      reef: FwewDarkReefTheme,
-    }
-  }
+  fwew: FwewTheme
 } as const;
 
-export const FwewThemeNames = ["fwew"] as const;
-export type FwewThemeName = typeof FwewThemeNames[number];
+type FwewThemeName = keyof typeof ThemeMap;
 
 type ThemeParams = {
   fwewTheme: FwewThemeName,
@@ -74,7 +17,7 @@ type ThemeParams = {
 
 export function getTheme({ fwewTheme, colorScheme, dialect }: ThemeParams): Theme {
   if (!colorScheme) {
-    return FwewLightTheme
+    colorScheme = "light";
   }
   return ThemeMap[fwewTheme][colorScheme][dialect];
 }
