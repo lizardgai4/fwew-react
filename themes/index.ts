16e41170306a702cbf25947b6765b397b794e8a7
Corey Scheideman <corscheid@gmail.com>
2024-09-25 01:51:33 -0500

    [WIP] Refactor theme system

--- themes/index.ts
+++ themes/index.ts
@@ -2,33 +2,14 @@ import FwewTheme from "@/themes/fwew";
 import { type Dialect } from "@/types/common";
 import { type ColorExtension } from "@/types/theme";
 import { type Theme } from "@react-navigation/native";
-import {
-  TextInputProps,
-  TextProps,
-  ViewProps,
-  type ColorSchemeName,
-} from "react-native";
-
-export type ThemeType = {
-  light: Record<Dialect, Theme>;
-  dark: Record<Dialect, Theme>;
-  ext: ColorExtension;
-  components: {
-    Text: (props: TextProps) => JSX.Element;
-    MonoText: (props: TextProps) => JSX.Element;
-    BoldText: (props: TextProps) => JSX.Element;
-    ItalicText: (props: TextProps) => JSX.Element;
-    UnderlinedText: (props: TextProps) => JSX.Element;
-    CardView: (props: ViewProps) => JSX.Element;
-    TextInput: (props: TextInputProps) => JSX.Element;
-  };
-};
+import { type ColorSchemeName } from "react-native";
 
 const ThemeMap = {
   fwew: FwewTheme,
-} as const;
+};
 
 export type ThemeName = keyof typeof ThemeMap;
+
 export const ThemeNames = Object.keys(ThemeMap) as ThemeName[];
 
 export function getTheme(
--- types/theme.ts
+++ types/theme.ts
@@ -1,3 +1,7 @@
+import type { Dialect } from "@/types/common";
+import type { Theme } from "@react-navigation/native";
+import type { TextInputProps, TextProps, ViewProps } from "react-native";
+
 export type ColorExtension = Record<
   "light" | "dark",
   {
@@ -11,3 +15,18 @@ export type ColorExtension = Record<
     innerCard: string;
   }
 >;
+
+export type ThemeType = {
+  light: Record<Dialect, Theme>;
+  dark: Record<Dialect, Theme>;
+  ext: ColorExtension;
+  components: {
+    Text: (props: TextProps) => JSX.Element;
+    MonoText: (props: TextProps) => JSX.Element;
+    BoldText: (props: TextProps) => JSX.Element;
+    ItalicText: (props: TextProps) => JSX.Element;
+    UnderlinedText: (props: TextProps) => JSX.Element;
+    CardView: (props: ViewProps) => JSX.Element;
+    TextInput: (props: TextInputProps) => JSX.Element;
+  };
+};
