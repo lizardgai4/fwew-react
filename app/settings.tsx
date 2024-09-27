9c971e73a29c56ec11374e50ee8c39ab4e348ec5
Corey Scheideman <corscheid@gmail.com>
2024-09-27 01:43:16 -0500

    Update Settings UI
    First screen to support Tablet/Landscape/Wide layout

--- app/settings.tsx
+++ app/settings.tsx
@@ -3,31 +3,46 @@ import { AppLanguageSelect } from "@/components/settings/AppLanguageSelect";
 import { DialectSelect } from "@/components/settings/DialectSelect";
 import { ResultsLanguageSelect } from "@/components/settings/ResultsLanguageSelect";
 import { ThemeSelect } from "@/components/settings/ThemeSelect";
-import { getUI } from "@/constants/i18n";
-import { useAppLanguageContext } from "@/context/AppLanguageContext";
-import { useDialectContext } from "@/context/DialectContext";
-import { useThemeNameContext } from "@/context/ThemeNameContext";
-import { getThemedComponents } from "@/themes";
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  useWindowDimensions,
+  View,
+} from "react-native";
 
 export default function SettingsScreen() {
-  const { appLanguage } = useAppLanguageContext();
-  const { dialect } = useDialectContext();
-  const ui = getUI(appLanguage, dialect);
-  const { themeName } = useThemeNameContext();
-  const Themed = getThemedComponents(themeName);
+  const { width } = useWindowDimensions();
+  const wide = width > 720;
+
+  const ratio =
+    [
+      { breakpoint: 1280, value: 3 },
+      { breakpoint: 950, value: 2 },
+      { breakpoint: 720, value: 1 },
+    ].filter((b) => width >= b.breakpoint)[0]?.value ?? 1;
 
   return (
     <ScrollView>
       <StatusBar style="light" />
-      <View style={styles.container}>
-        <AppLanguageSelect />
-        <ResultsLanguageSelect />
-        <DialectSelect />
-        <ThemeSelect />
-        <Themed.Text style={styles.value}>{ui.settings.about}</Themed.Text>
-        <About />
+      <View
+        style={[
+          styles.container,
+          {
+            flexDirection: wide ? "row" : "column",
+            justifyContent: wide ? "center" : undefined,
+          },
+        ]}
+      >
+        <View style={{ flex: 1, gap: 16 }}>
+          <AppLanguageSelect />
+          <ResultsLanguageSelect />
+          <DialectSelect />
+          <ThemeSelect />
+        </View>
+        <View style={{ flex: ratio, gap: 16 }}>
+          <About />
+        </View>
       </View>
     </ScrollView>
   );
--- components/settings/About.tsx
+++ components/settings/About.tsx
@@ -1,5 +1,8 @@
 import { Credits } from "@/components/settings/Credits";
 import { Version } from "@/components/settings/Version";
+import { getUI } from "@/constants/i18n";
+import { useAppLanguageContext } from "@/context/AppLanguageContext";
+import { useDialectContext } from "@/context/DialectContext";
 import { useThemeNameContext } from "@/context/ThemeNameContext";
 import { getThemedComponents } from "@/themes";
 import { StyleSheet } from "react-native";
@@ -7,9 +10,13 @@ import { StyleSheet } from "react-native";
 export function About() {
   const { themeName } = useThemeNameContext();
   const Themed = getThemedComponents(themeName);
+  const { appLanguage } = useAppLanguageContext();
+  const { dialect } = useDialectContext();
+  const ui = getUI(appLanguage, dialect);
 
   return (
     <Themed.CardView style={styles.expanded}>
+      <Themed.Text style={styles.value}>{ui.settings.about}</Themed.Text>
       <Version />
       <Credits />
     </Themed.CardView>
@@ -23,8 +30,9 @@ const styles = StyleSheet.create({
     gap: 16,
   },
   value: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "bold",
+    paddingBottom: 8,
   },
   expanded: {
     padding: 16,
