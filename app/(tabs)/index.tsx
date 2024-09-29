7156318235cc0a7c2ae4d429baa2c3507ea9e934
Corey Scheideman <corscheid@gmail.com>
2024-09-28 23:44:21 -0500

    Update Search page layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,13 @@ import { useAppLanguageContext } from "@/context/AppLanguageContext";
 import { useDialectContext } from "@/context/DialectContext";
 import { useFwew } from "@/hooks/useFwew";
 import { useTheme } from "@react-navigation/native";
-import { RefreshControl, ScrollView, StyleSheet, View } from "react-native";
+import {
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  useWindowDimensions,
+  View,
+} from "react-native";
 
 export default function SearchScreen() {
   const {
@@ -25,19 +31,36 @@ export default function SearchScreen() {
   const { appLanguage } = useAppLanguageContext();
   const { dialect } = useDialectContext();
   const ui = getUI(appLanguage, dialect).search;
+  const { width } = useWindowDimensions();
+  const wide = width > 720;
+  const ratio =
+    [
+      { breakpoint: 1280, value: 3 },
+      { breakpoint: 950, value: 2 },
+      { breakpoint: 720, value: 1 },
+    ].filter((b) => width >= b.breakpoint)[0]?.value ?? 1;
 
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="always"
-      refreshControl={
-        <RefreshControl
-          refreshing={loading}
-          onRefresh={execute}
-          colors={[colors.primary]}
-        />
-      }
+    // <ScrollView
+    //   keyboardShouldPersistTaps="always"
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={loading}
+    //       onRefresh={execute}
+    //       colors={[colors.primary]}
+    //     />
+    //   }
+    // >
+    <View
+      style={[styles.container, { flexDirection: wide ? "row" : "column" }]}
     >
-      <View style={styles.container}>
+      <View
+        style={{
+          flex: wide ? 1 : undefined,
+          padding: 16,
+          paddingBottom: 0,
+        }}
+      >
         <SearchBar
           query={query}
           search={search}
@@ -51,19 +74,37 @@ export default function SearchScreen() {
           value={naviOnly}
           onValueChange={setNaviOnly}
         />
-        <ResultCount
-          visible={query.length > 0 && resultCount > 0}
-          resultCount={resultCount}
-        />
-        <FwewSearchResults loading={loading} results={results} />
       </View>
-    </ScrollView>
+      <View style={{ flex: ratio }}>
+        <View style={{ padding: 16, paddingTop: wide ? 16 : 0 }}>
+          <ResultCount
+            visible={query.length > 0 && resultCount > 0}
+            resultCount={resultCount}
+          />
+        </View>
+        <ScrollView
+          keyboardShouldPersistTaps="always"
+          refreshControl={
+            <RefreshControl
+              refreshing={loading}
+              onRefresh={execute}
+              colors={[colors.primary]}
+            />
+          }
+        >
+          <View style={{ padding: 16, paddingTop: 0 }}>
+            <FwewSearchResults loading={loading} results={results} />
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+    /* </ScrollView> */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    gap: 16,
   },
 });
